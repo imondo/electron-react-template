fa5e6b00c947f5bb314f2bbefdeda916b43bd70a
Mondo <568197478@qq.com>
2018-09-26 16:21:40 +0800

    程序的增改删查

---
+++ src/views/index/List.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import { List, Icon, Input } from 'antd';
+
+class ListItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dataSource: [],
+      editList: {}
+    };
+  }
+  deleteItem = index => {
+    this.props.handleRemove(index);
+  }
+  editItem = index => {
+    let { dataSource } = this.state;
+    let data = dataSource.map((v, i) => {
+      if (i === index) {
+        v.isEdit = true;
+      }
+      return v;
+    });
+    this.setState({dataSource: data});
+  }
+  setEditValue = e => {
+    let { value, dataset: { index } } = e.target;
+    let { editList } = this.state;
+    editList[index] = value;
+    this.setState({editList});
+  }
+  saveItem = index => {
+    let { dataSource, editList } = this.state;
+    let data = dataSource.map((v, i) => {
+      if (i === Number(index)) {
+        v.isEdit = false;
+        v.val = editList[index];
+        delete editList[index];
+      }
+      return v;
+    });
+    this.setState({ editList });
+    this.props.handleEdit(data);
+  }
+  componentWillReceiveProps(nextProps) {
+    let { data } = nextProps;
+    let dataSource = data.map(v => {
+      let obj = {
+        val: v,
+        isEdit: false
+      }
+      return obj;
+    });
+    this.setState({ dataSource });
+  }
+  render() {
+    let { dataSource } = this.state;
+
+    return (      
+      <List
+        className="list-cover"
+        dataSource={dataSource}
+        renderItem={(item, index) => {
+          const actions = [
+            <Icon type="edit" theme="outlined"  onClick={this.editItem.bind(this, index)}/>,
+            <Icon type="delete" theme="outlined" onClick={this.deleteItem.bind(this, index)}/>
+          ];
+
+          const saveActions = [
+            <Icon type="check" theme="outlined" onClick={this.saveItem.bind(this, index)}/>,
+            <Icon type="delete" theme="outlined" onClick={this.deleteItem.bind(this, index)}/>
+          ];
+
+          if (item.isEdit) {
+            return (
+              <List.Item actions = {saveActions}>
+                <Input 
+                  type="text" 
+                  defaultValue={item.val} 
+                  data-index={index} 
+                  onChange={this.setEditValue}/>
+              </List.Item>
+            );
+          } else {
+            return <List.Item actions = {actions}>{item.val}</List.Item>;
+          }
+          
+        }}
+      />
+    );
+  }
+} 
+
+export default ListItem;
--- src/views/index/index.js
+++ src/views/index/index.js
@@ -1,11 +1,72 @@
 import React, { Component } from 'react';
+import { Input, Button } from 'antd';
+import ListItem from './List';
 import './index.less';
 
+const Search = Input.Search;
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      val: '',
+      list: [],
+      searchList: []
+    }
+  }
+  getValue = (e) => {
+    this.setState({ val: e.target.value });
+  }  
+  handleSearch = value => {
+    if (value) {
+      let { list } = this.state;
+      let data = list.filter(v => v.includes(value));
+      this.setState({ searchList: data });
+    } else {
+      this.setState({ searchList: [] });
+    }
+  }  
+  handleAdd = () => {
+    let { val } = this.state;
+    if (val) {
+      let { list } = this.state;
+      this.setState({ list });
+      this.refs.searchInput.input.input.value = '';
+    }
+  }
+  handleRemove = index => {
+    let { list } = this.state;
+    list.splice(index, 1);
+    this.setState({ list });
+  }
+  handleEdit = data => {
+    let list = data.reduce((arr, v) => {
+      arr.push(v.val);
+      return arr;
+    }, []);
+    this.setState({ list });
+  }
   render() {
+    let { list, searchList } = this.state;
+    let data = searchList.length ? searchList : list;
+    console.log(data);
     return (
-      <div className="App">
+      <div className="index-cover">
         <h3>electron简单任务列表</h3>
+        <div>
+          <Search
+            ref="searchInput"
+            placeholder="请输入任务"
+            onChange={this.getValue}
+            onSearch={this.handleSearch}
+          />
+          <Button className="btn-add" type="primary" onClick={this.handleAdd}>新建</Button>
+        </div>
+        <ListItem 
+          data={ data } 
+          handleRemove={this.handleRemove.bind(this)}
+          handleEdit={this.handleEdit.bind(this)} />
       </div>
     );
   }
